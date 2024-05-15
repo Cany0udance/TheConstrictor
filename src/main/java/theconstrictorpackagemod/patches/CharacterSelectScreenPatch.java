f44e40598ca4bebba4bb68d6b616855cf33bf929
Cany0udance <blakkrum@aol.com>
2024-05-14 23:32:51 -0400

    stuff

--- src/main/java/theconstrictorpackagemod/patches/CharacterSelectScreenPatch.java
+++ src/main/java/theconstrictorpackagemod/patches/CharacterSelectScreenPatch.java
@@ -2,6 +2,7 @@ package theconstrictorpackagemod.patches;
 
 import characterclass.MyCharacter;
 import com.badlogic.gdx.graphics.Texture;
+import com.evacipated.cardcrawl.modthespire.lib.*;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.core.Settings;
 import com.megacrit.cardcrawl.helpers.FontHelper;
@@ -13,9 +14,6 @@ import com.megacrit.cardcrawl.screens.charSelect.CharacterOption;
 import com.megacrit.cardcrawl.screens.charSelect.CharacterSelectScreen;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.evacipated.cardcrawl.modthespire.lib.SpirePatch;
-import com.evacipated.cardcrawl.modthespire.lib.SpirePostfixPatch;
-import com.evacipated.cardcrawl.modthespire.lib.SpireField;
 import theconstrictorpackagemod.theconstrictormod;
 
 import java.lang.reflect.Field;
