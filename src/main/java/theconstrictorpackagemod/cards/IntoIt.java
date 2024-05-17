04518adc885aefa8d3eae672f1f4b76008930d63
Cany0udance <blakkrum@aol.com>
2024-05-17 00:31:14 -0400

    stuff

--- src/main/java/theconstrictorpackagemod/cards/IntoIt.java
+++ src/main/java/theconstrictorpackagemod/cards/IntoIt.java
@@ -1,18 +1,9 @@
 package theconstrictorpackagemod.cards;
 
 import characterclass.MyCharacter;
-import com.megacrit.cardcrawl.actions.AbstractGameAction;
-import com.megacrit.cardcrawl.actions.animations.VFXAction;
-import com.megacrit.cardcrawl.actions.common.DamageAction;
-import com.megacrit.cardcrawl.actions.common.GainBlockAction;
-import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
-import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
-import com.megacrit.cardcrawl.powers.AbstractPower;
-import com.megacrit.cardcrawl.vfx.combat.VerticalImpactEffect;
 import theconstrictorpackagemod.actions.IntoItAction;
-import theconstrictorpackagemod.powers.ConstrictingPower;
 import theconstrictorpackagemod.util.CardInfo;
 
 import static theconstrictorpackagemod.theconstrictormod.makeID;
