e4f833d18ab1f26a22b398eb498cb31cbedbc82c
Cany0udance <blakkrum@aol.com>
2024-05-16 22:40:54 -0400

    muddy water in gremlin match instead of lil hit

--- src/main/java/characterclass/MyCharacter.java
+++ src/main/java/characterclass/MyCharacter.java
@@ -135,7 +135,7 @@ public class MyCharacter extends CustomPlayer {
     public AbstractCard getStartCardForEvent() {
         //This card is used for the Gremlin card matching game.
         //It should be a non-strike non-defend starter card, but it doesn't have to be.
-        return new MyCard();
+        return new MyCard2();
     }
 
     /*- Below this is methods that you should *probably* adjust, but don't have to. -*/
