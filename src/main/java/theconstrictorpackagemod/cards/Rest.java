ee16eb413c025c7a7c2bbbff7454f6bd4f924917
Cany0udance <blakkrum@aol.com>
2024-05-20 22:20:23 -0400

    rest

---
+++ src/main/java/theconstrictorpackagemod/cards/Rest.java
@@ -0,0 +1,34 @@
+package theconstrictorpackagemod.cards;
+
+import characterclass.MyCharacter;
+import com.megacrit.cardcrawl.actions.common.MakeTempCardInHandAction;
+import com.megacrit.cardcrawl.actions.unique.RetainCardsAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import theconstrictorpackagemod.util.CardInfo;
+
+import static theconstrictorpackagemod.theconstrictormod.makeID;
+
+public class Rest extends BaseCard {
+    private final static CardInfo cardInfo = new CardInfo(
+            "Rest",
+            1,
+            CardType.SKILL,
+            CardTarget.SELF,
+            CardRarity.UNCOMMON,
+            MyCharacter.Enums.CARD_COLOR);
+
+    public static final String ID = makeID(cardInfo.baseId);
+
+    public Rest() {
+        super(cardInfo);
+        this.exhaust = true;
+        setMagic(1,1);
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        this.addToBot(new RetainCardsAction(p, magicNumber));
+
+    }
+}
